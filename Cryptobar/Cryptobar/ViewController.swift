b5259995c850b29766534849b420482c0a26db28
commit-master <paul.devops@gmail.com>
2017-09-01 14:12:08 +0200

    * : init the project

---
+++ Cryptobar/Cryptobar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Cryptobar
+//
+//  Created by Paul Borensztein on 01/09/2017.
+//  Copyright © 2017 Bakaking. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
