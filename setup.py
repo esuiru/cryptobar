87659a25479858994a5d500cc88b24a21dff8363
Paul Borensztein <paul.devops@gmail.com>
2017-09-09 17:28:06 +0200

    * : created install scripts

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""
+# CODE BY: commit-master 🚀
+# MADE ON: Saturday, September 9, 2017
+# Lorem ipsum et dolor amet. Su colores et un ich bin un berliner ya
+# du bist la description of el project right there.
+"""
+
+from setuptools import setup
+
+APP = ['main.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    'plist': {
+        'LSUIElement': True,
+    },
+    'packages': ['rumps', 'requests'],
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
