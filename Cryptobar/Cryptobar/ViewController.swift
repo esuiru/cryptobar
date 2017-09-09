4f5ef5d182a80de3b612cef268c7b86d97e53bb7
Paul Borensztein <paul.devops@gmail.com>
2017-09-09 16:05:35 +0200

    * : added base project files

---
+++ Cryptobar/Cryptobar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Cryptobar
+//
+//  Created by Paul Borensztein on 09/09/2017.
+//  Copyright © 2017 cryptonaire. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
