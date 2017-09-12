d76f86efde97d605330d00821f072ffc7fd46c41
commit-master <paul.devops@gmail.com>
2017-09-12 11:23:37 +0200

    * : added fluctuation

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import rumps
 import requests
 
 API_URL = 'https://api.coinmarketcap.com/v1/ticker/'
+VALUES = {}
 
 # Change the values of this array to the coins you are currently tracking.
 # You can change the FIAT variable to the desired currency you want to convert
@@ -20,7 +21,7 @@ def fetch_api(coin):
     """ This function goes and fetch us the latest coins data """
     resp = requests.get(API_URL + coin + '/?convert=' + FIAT['value'])
     for key in resp.json():
-        return [key["symbol"], key["price_usd"] + FIAT['symbol']]
+        return [key["symbol"], float(key["price_usd"])]
 
 class Cryptobar(rumps.App):
     """ This is the core component of the app, this class will be used to build
@@ -32,12 +33,23 @@ class Cryptobar(rumps.App):
     def ticker(self, _):
         """ This ticker function allows us to update the price in the StatusBar
         every seconds to stay on top of the latest charts. """
+        global VALUES
+        previous_values = dict(VALUES)
 
-        k = []
         for coin in CURRENCIES:
-            k.append(fetch_api(coin))
+            VALUES[coin] = fetch_api(coin)
+            previous_values.setdefault(coin, VALUES[coin])
 
-        self.title = ' ⚡️ '.join("{0}: {1}".format(x[0], x[1]) for x in k)
+            fluctuation = '😱' if previous_values[coin][1] > VALUES[coin][1] \
+                          else '🚀' if previous_values[coin][1] < VALUES[coin][1] \
+                          else '😐'
+
+            VALUES[coin].append(fluctuation)
+
+        self.title = '▲  '.join("{0}: {1}{2} 〈{3}〉".format(value[0],
+                                                            value[1],
+                                                            FIAT['symbol'],
+                                                            value[2]) for value in VALUES.values())
 
 if __name__ == "__main__":
     Cryptobar().run()
